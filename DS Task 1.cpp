587a48af678ceddfce29912521cea4f81357ce5e
VladimirSyrinx <VladimirSyrinx@users.noreply.github.com>
2020-04-29 19:35:07 +0700

    Initial commit (CURRENTLY PROGRAM DOES NOT WORK)

--- DS Task 1.cpp
+++ DS Task 1.cpp
@@ -1,6 +1,50 @@
-#include <iostream>
+///   3.1.Односвязный список
+///   1. Задан текст, состоящий из строк, разделенных пробелом и оканчивающийся точкой.
+///   Написать подпрограмму поиска заданного элемента в списке.Используя эту подпрограмму :
+///   а) подсчитать количество вхождений заданного символа в каждую строку текста.
+///   Вхождение задавать номером строки и номером позиции в строке;
+
+#include <iomanip>
+#include <fstream>
+
+struct TextString
+{
+   TextString *p_nextSymbol = NULL;
+   char symbolVal = '\0';
+} *headList;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   std::fstream in("input.txt");
+   std::ofstream out("putput.txt");
+
+   TextString *headList = new TextString;
+
+   const char POINT_SYMBOL = '.';
+
+   char tmpSymbol = '\0';
+   TextString *p_tmpSymbol = headList;
+   while (tmpSymbol != POINT_SYMBOL)
+   {
+      p_tmpSymbol->p_nextSymbol = new TextString;
+      p_tmpSymbol = p_tmpSymbol->p_nextSymbol;
+      in >> tmpSymbol;
+      p_tmpSymbol->symbolVal = tmpSymbol;
+   }
+   tmpSymbol = '\0';
+   p_tmpSymbol = headList;
+
+   const char ENTER_SYMBOL = '\n';
+
+   char wantedSymbol = '\0';
+   in >> wantedSymbol;
+
+   size_t charCnt = 0;
+   size_t strCnt = 1;
+   while (tmpSymbol != POINT_SYMBOL)
+   {
+
+   }
+
+   return 0;
 }
